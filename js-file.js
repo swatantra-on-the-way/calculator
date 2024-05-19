f40989b604472ae7db1bab0343b75f5c774c259c
swatantra-on-the-way <swatantrachhetri3@gmail.com>
2024-05-19 20:51:20 +1000

    Make the calculator perform basic functions

--- js-file.js
+++ js-file.js
@@ -14,29 +14,63 @@ function divide(num1, num2){
     return num1 / num2;
 }
 
-function splitOperation(operation) {
-    let arr = operation.split(" ");
+function splitOperation(expr) {
+    let arr = expr.split(" ");
+    if (arr.length == 1){
+        return +arr[0];
+    }
     let num1 = +arr[0];
     let num2 = +arr[2];
     let operator = arr[1];
-    operate(operator, num1, num2);
+    return operate(operator, num1, num2);
 }
 
 function operate(operator, num1, num2){
     if (operator === "+"){
-        let result = add(num1, num2);
+        return add(num1, num2);
     }
     else if (operator === "-"){
-        let result = subtract(num1, num2);
+        return subtract(num1, num2);
     }
     else if (operator === "*"){
-        let result = multiply(num1, num2);
+        return multiply(num1, num2);
     }
     else if (operator === "/"){
-        let result = divide(num1, num2);
+        return divide(num1, num2);
     }
     else{
-        let result = "Please recheck your operator value.";
+        return "Please recheck your operator value.";
     }
+    return result;
 }
+screen = document.querySelector(".screen");
+let input = screen.textContent;
+displayValue = "";
+addListenerToButtons();
+
+
+function addListenerToButtons() {
+    keys = document.querySelectorAll(".key");
+    keys.forEach((key) => {
+        key.addEventListener("click", (event) => {
+            if(key.textContent === "+" || key.textContent === "-" || key.textContent === "*" || key.textContent === "/"){
+                let result = splitOperation(displayValue);
+                displayValue = result + " " + key.textContent + " ";
+            }
+            else if(key.textContent === "="){
+                let result = splitOperation(displayValue);
+                displayValue = result;
+            }
+            else if (key.textContent === "CLS"){
+                displayValue = "";
+            }
+            else{
+                displayValue += key.textContent;
+            }
+            screen.textContent = displayValue;
+        })
+
+    })
+}
+
 
