6ff9918b8e3b8af0b95efeaf54ec102439327041
swatantra-on-the-way <swatantrachhetri3@gmail.com>
2024-05-22 19:37:46 +1000

    Add functionality to populate display after click
    Event listeners are added to numbered buttons so that every time a number is clicked, it just displays and concatenates into the previous display content.

--- js-file.js
+++ js-file.js
@@ -17,3 +17,33 @@ function divide(num1, num2){
 let num1 = 0;
 let num2 = 0;
 let op = "";
+let displayValue = "";
+let screen = document.querySelector(".screen");
+
+function operate(num1, num2, op){
+    let result = 0;
+    if (op === "+"){
+        result = add(num1, num2);
+    }
+    else if (op === "-"){
+        result = add(num1, num2);
+    }
+    else if (op === "*"){
+        result = multiply(num1, num2);
+    }
+    else if (op === "/"){
+        result = divide(num1, num2);
+    }
+    return result.toString();
+
+}
+
+let buttons = document.querySelectorAll("button");
+buttons.forEach((button) => button.addEventListener("click", (event) => {
+    if (button.className === "numKey"){
+        console.log("hi");
+        displayValue += event.target.textContent;
+        screen.textContent = displayValue;
+
+    }
+}));
