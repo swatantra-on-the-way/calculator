3cde620e296873bef3b31225afdf14be5c3aad76
swatantra-on-the-way <swatantrachhetri3@gmail.com>
2024-05-25 07:10:37 +1000

    Make your calc work when +/- entered at beginning
    For example, +2 + 3 = 5, -2 + 3 = 1 and so on

--- js-file.js
+++ js-file.js
@@ -17,6 +17,8 @@ function divide(num1, num2){
 let displayValue = "";
 let screen = document.querySelector(".screen");
 let prevKey ="";
+let prevValue = "";
+let sign = 0;
 
 function operate(num1, num2, op){
     let result = 0;
@@ -24,7 +26,7 @@ function operate(num1, num2, op){
         result = add(num1, num2);
     }
     else if (op === "-"){
-        result = add(num1, num2);
+        result = subtract(num1, num2);
     }
     else if (op === "*"){
         result = multiply(num1, num2);
@@ -43,7 +45,14 @@ buttons.forEach((button) => button.addEventListener("click", (event) => {
             displayValue = event.target.textContent;
         }
         else {
-            displayValue += event.target.textContent;
+            if (sign) {
+                displayValue = Number(prevValue + event.target.textContent).toString();
+                sign = 0;
+            }
+            else {
+                displayValue += event.target.textContent;
+
+            }
         }
 
     }
@@ -54,13 +63,21 @@ buttons.forEach((button) => button.addEventListener("click", (event) => {
         if (no_of_spaces == 2){
             displayValue = operate(Number(arr[0]), Number(arr[2]), arr[1]);
         }
-    }
 
-    //Add spacing only if it's an operation key
-    if (event.target.className == "opKey"){
+        //Add spacing only if it's an operation key
+        if (event.target.className == "opKey"){
+            if ((event.target.textContent == "+" || event.target.textContent == "-") && displayValue == ""){
+                sign = 1;
+             }
             displayValue += " " + event.target.textContent + " ";
         }
-    
+    }
+
+    else if(event.target.className == "clearKey"){
+        displayValue = "";
+    }
+
     prevKey = event.target.className;
+    prevValue = event.target.textContent;
     screen.textContent = displayValue;
 }));
